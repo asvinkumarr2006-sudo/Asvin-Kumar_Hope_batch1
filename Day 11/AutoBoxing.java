82c6dc65c1aa0c1944702bea33e5876ee21dc3d8
asvinkumarr2006-sudo <asvinkumarr2006@gmail.com>
2026-04-06 22:24:04 +0530

    Add files via upload

---
+++ Day 11/AutoBoxing.java
@@ -0,0 +1,7 @@
+public class AutoBoxing {
+        public static void main(String[] args) {
+        Float a=20.0f; //AutoBoxing
+        Float b=20.0f;
+        System.out.println(a==b);
+    }    
+}
